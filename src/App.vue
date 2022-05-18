130c2da7533d2adb16db1f42656b851c19f98204
Yuvraj883 <kingyuvi88@gmail.com>
2022-05-18 23:12:38 +0530

    Added About page

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <Nav/>
+  <About/>
 </template>
 
 <script>
 import Nav from './components/Nav.vue';
+import About from './components/About.vue'
 
 export default {
 //eslint-disable-multi-word
 
   name: 'App',
   components: {
-    Nav
+    Nav,
+    About
   }
 }
 </script>
@@ -23,5 +26,13 @@ export default {
   text-align: center;
   color: #2c3e50;
  
+}
+Html{
+  margin:0;
+  padding:0;
+}
+body{
+  margin:0;
+  padding:0;
 }
 </style>
---
+++ src/components/About.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="container">
+    <div class="number">
+      <img src="../assets/SOLID-NUMBERS/2.png" alt="1" />
+    </div>
+    <div class="about" >
+        <div>
+        <img src="../assets/LoveDeep.jpeg" alt="LoveDeep"/>
+        <p class="name">Love Deep kashyap</p>
+        <p class="title">Founder, Deep Fake Company </p>
+        <p>All I need is 1 verse to put you in a hearse, and I don't even have to put my cap on (Naa)
+Kalamkaar ko bana baitha main deathrow
+Iss baar iski maut tull na paaye jaise Jethro
+Ending the silence with violins of violence
+Now watch a dead rapper get fucked, no necro
+Kulfa kya karegi kulfi hai khud, Bunty toh lagta hai buddi se dusht
+Automatic tha brush, automatic ab tange
+Jab dikhta hai dollar toh khulti hai khud (Khud)
+Dekho ye chapri hai, soch iski naali me (Naali me)
+Isko laga ye Brutal hai gaali se (Gaali se)
+Mera gaana ni chala tha tere naam se
+Chala mera gaana teri badnaami se</p>
+</div>
+
+    </div>
+  </div>
+</template>
+<script>
+export default{
+    name:'About'
+}
+</script>
+<style scoped>
+.container {
+  background-image: url("../assets/SOLID-NUMBERS/gradient-background2.jpg");
+  color: white;
+  margin: 0;
+  display: flex;
+}
+.title{
+    margin-top:1vh;
+}
+.name{
+    font-weight: bolder;
+    font-size:1.5em;
+    margin-bottom: 0vh;
+}
+.about div{
+    margin-top: 10vh;
+    width: 80%;
+    margin-left:5vw;
+
+}
+.number{
+   
+    width:40%;
+    margin-right:5%;
+    margin-top:5%;
+    
+}
+.number img{
+    height:50vh;
+}
+.about img{
+    border-radius:50%;
+    height:10vh;
+}
+.about{
+    padding:5vh;
+      width:60%;
+    display:block;
+    margin-left:7%;
+}
+</style>
