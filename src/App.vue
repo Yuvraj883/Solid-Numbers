f083305a52ed18cf85d9500e7961c360c889a034
Yuvraj883 <kingyuvi88@gmail.com>
2022-05-18 17:25:42 +0530

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Nav/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Nav from './components/Nav.vue';
 
 export default {
+//eslint-disable-multi-word
+
   name: 'App',
   components: {
-    HelloWorld
+    Nav
   }
 }
 </script>
@@ -21,6 +22,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+ 
 }
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,89 @@
+<template>
+<div class="container">
+<div class="nav">
+    <span class="logo">
+        Solid Numbers
+    </span>
+    
+    <ul class="right-align">
+       <li>About</li>
+       <li>Team</li>
+       <li>FAQ</li>
+       <li>Mint</li>
+    </ul>
+
+    
+</div>
+<div class="content">
+<div class="solid">
+<img src="../assets/SOLID-NUMBERS/solid-numbers.png" alt="Solid Numbers">
+<button class="mint">Mint Now</button>
+</div>
+<div class="number">
+    <img src="../assets/SOLID-NUMBERS/1.png" alt="1">
+</div>
+</div>
+</div>
+</template>
+<script>
+export default{
+    name:'Nav'
+}
+</script>
+<style scoped>
+.mint{
+  
+    color:black;
+    border-radius: 1em;
+    padding:.2em .6em;
+    margin-right:60%;
+    font-size:1.5em;
+    margin-bottom:1em;
+    margin-bottom:1em;
+
+}
+
+.content{
+    display:flex;
+}
+.solid img{
+   
+    width:60%;
+    display:block;
+    margin-left:7%;
+}
+.number img{
+   
+    width:40%;
+    margin-right:5%;
+    margin-top:5%;
+    
+}
+.container{
+    background-image: url("../assets/SOLID-NUMBERS/gradient-background.jpg");
+    color:white;
+    margin:0;
+
+}
+.right-align{
+    text-align:right;
+    display:flex;
+    margin-left:auto;
+    margin-right:5%;
+
+    
+}
+
+.nav{
+    display:flex;
+    
+}
+li{
+    list-style: none;
+    margin:1em;
+}
+.logo{
+    margin-top:1em;
+    margin-left:7%;
+}
+</style>
